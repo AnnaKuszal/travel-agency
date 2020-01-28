9b04236e9d15753782281ec7d67858454d5824b0
Anna Kuszal <anna.kuszal@o2.pl>
2020-01-28 17:33:13 +0100

    Implement function formatTime()

--- src/utils/formatTime.js
+++ src/utils/formatTime.js
@@ -1,1 +1,15 @@
-export const formatTime = () => 'formatted time';
+export const formatTime = (arg) => { 
+  if(arg == undefined ||isNaN(arg) || typeof(arg) == 'function' || arg < 0) {
+    return null;
+  }
+  else {
+    let input = arg;
+
+    let seconds = Math.floor(input%60).toString();
+    let minutes = Math.floor((input/60)%60).toString();
+    let hours = Math.floor(input/3600).toString();
+
+    
+    return hours.padStart(2, '0') + ':' + minutes.padStart(2, '0') + ':' + seconds.padStart(2, '0');
+  }
+};
